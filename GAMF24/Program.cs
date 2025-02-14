1fb4a3599737188800bec0d71c3206e15cc6464a
Busai Dániel <busai.dani@gmail.com>
2025-02-14 08:16:15 +0100

    Made some changes to code

--- GAMF24/Program.cs
+++ GAMF24/Program.cs
@@ -1,54 +1,48 @@
-﻿//using System.Linq;
+﻿const string DIR = "H:\\gamf";
+List<int> osztok = new List<int>();
+int num = 1310438493;
+int goodNums = 0;
+for (int i = 1; i < Math.Sqrt(num); i++)
+{
+    if (num % i == 0)
+    {
+        osztok.Add(i);
+    }
+}
 
-const string DIR = "H:\\gamf";
-//List<int> osztok = new List<int>();
-//int num = 1310438493;
-//int goodNums = 0;
-//for (int i = 1; i < Math.Sqrt(num); i++)
-//{
-//    if (num % i == 0)
-//    {
-//        osztok.Add(i);
-//    }
-//}
+using (StreamReader stream = new StreamReader($"{DIR}\\szamok.txt"))
+{
+    while (!stream.EndOfStream)
+    {
+        long line = long.Parse(stream.ReadLine());
+        int counter = 0;
+        for (long i = 1; i <= Math.Sqrt(line); i++)
+        {
+            if (line % i == 0)
+            {
+                if (osztok.ToHashSet().Contains((int)i))
+                {
+                    counter++;
+                }
+            }
 
-//using (StreamReader stream = new StreamReader($"{DIR}\\szamok.txt"))
-//{
-//    while(!stream.EndOfStream)
-//    {
-//        long line = long.Parse(stream.ReadLine());
-//        Console.WriteLine(line);
-//        int counter = 0;
-//        for (long i = 1; i <= Math.Sqrt(line); i++)
-//        {
-//            if (line % i == 0)
-//            {
-//                if(osztok.ToHashSet().Contains((int)i))
+            if (counter > 1)
+            {
+                break;
+            }
+        }
+        if (counter == 1)
+        {
+            goodNums++;
+        }
+        counter = 0;
+    }
 
-//                if (osztok.Contains((int)i))
-//                {
-//                    counter++;
-//                }
-//            }
+}
 
-//            if (counter > 1)
-//            {
-//                break;
-//            }
-//        }
-//        if(counter == 1)
-//        {
-//            goodNums++;
-//        }
-//        counter = 0;
-//        Console.WriteLine(goodNums);
-//    }
+Console.WriteLine($"Második feladat: {goodNums - 1}");
 
-//}
-
-//Console.WriteLine(goodNums);
-
-//2. feladat
+//2.feladat
 
 string szam = "2354211341";
 char[] chars = szam.ToCharArray();
@@ -69,9 +63,8 @@ using (StreamReader sr = new($"{DIR}\\szamok.txt"))
         Array.Sort(charsLine);
         if (chars.SequenceEqual(charsLine))
         {
-            Console.WriteLine(line);
             annagrams++;
         }
     }
 }
-Console.WriteLine(annagrams - 1);
+Console.WriteLine($"Második feladat: {annagrams - 1}");
